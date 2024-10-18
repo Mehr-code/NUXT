131570dbc8f53266aa7f6f5b180826cdd2d30ae1
Mehr-code <papa.molla8@gmail.com>
2024-10-18 22:47:46 +0330

    commit

---
+++ Nuxt 3 Crash Course/nuxt-dojo/pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga quae eveniet
+      eligendi, iure fugit provident similique magnam doloribus fugiat
+      reiciendis? Dolores, quia.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga quae eveniet
+      eligendi, iure fugit provident similique magnam doloribus fugiat
+      reiciendis? Dolores, quia.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
