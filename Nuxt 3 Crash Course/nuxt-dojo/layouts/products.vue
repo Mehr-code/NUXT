f10464b30d0e2802139dc7306701517851860962
Mehr-code <papa.molla8@gmail.com>
2024-10-19 22:13:47 +0330

    commit

---
+++ Nuxt 3 Crash Course/nuxt-dojo/layouts/products.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <nuxt-link to="/">Nuxt Dojo Merch</nuxt-link>
+      </nav>
+    </header>
+
+    <!-- output the page content -->
+    <div>
+      <slot />
+    </div>
+    <footer>
+      <ul>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/products">Products</NuxtLink></li>
+      </ul>
+    </footer>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.router-link-exact-active {
+  color: #12b488;
+}
+
+p {
+  margin: 20px 0;
+}
+</style>
---
+++ Nuxt 3 Crash Course/nuxt-dojo/pages/products/[id].vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro quasi quae
+      voluptas labore atque qui sunt laborum nemo aliquam, incidunt eius
+      commodi, placeat dolorum, sapiente soluta perspiciatis architecto quis
+      omnis?
+    </p>
+  </div>
+</template>
+
+<script setup>
+const { id } = useRoute().params;
+definePageMeta({
+  layout: "products",
+});
+</script>
+
+<style scoped></style>
