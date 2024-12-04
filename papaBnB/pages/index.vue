1ad522626a6b5b57b6cb0203db2f5aaa23018ada
Mehr-code <papa.molla8@gmail.com>
2024-12-04 22:40:07 +0330

    commit

--- papaBnB/pages/index.vue
+++ papaBnB/pages/index.vue
@@ -1,7 +1,30 @@
 <template>
-  <div>hello world</div>
+  <div>
+    <div
+      v-for="home in homes"
+      :key="home.objectID"
+      style="float: left; margin: 10px"
+    >
+      <img :src="home.images[0]" alt="" style="width: 200px" /><br />
+      {{ home.title }}<br />
+      {{ home.location.address }}{{ home.location.city }}{{ home.location.state
+      }}<br />
+      {{ home.guests }} guests<br />
+      {{ home.pricePerNight }} / night <br />
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script>
+import homes from "~/data/homes";
+
+export default {
+  data() {
+    return {
+      homes: homes.slice(0, 3),
+    };
+  },
+};
+</script>
 
 <style scoped></style>
